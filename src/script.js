229479bb723c61ef44007cc5ddd2bce1bc1d55a4
CarolinaMacdian <115894648+CarolinaMacdian@users.noreply.github.com>
2022-11-13 18:46:33 +0000

    icon update

--- src/script.js
+++ src/script.js
@@ -51,7 +51,12 @@ function showWeather(response) {
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed * 3.6
   );
-  /**document.querySelector("#icon").innerHTML = response.data.weather.[0].icon;*/
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function convertToFahrenheit(event) {
@@ -108,11 +113,3 @@ let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
 
 searchCity("Porto");
-/**
- * 
- * function showCurrentIcon(event) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-  let apiUrl = `https://api.openweathermap.org/img/wn/${response.data.weather.[0].icon}@2x.png`;
-  axios.get(apiUrl).then(showWeather);
-}
- */
